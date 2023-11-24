2c9cc5f0da4fffa3e1469f14969939e719e65b70
KS <kangsheng@mindvalley.com>
2023-11-24 14:37:04 +0800

    add amount_excluding_tax to invoice line item

--- mix.exs
+++ mix.exs
@@ -2,7 +2,7 @@ defmodule Stripe.Mixfile do
   use Mix.Project
 
   @source_url "https://github.com/code-corps/stripity_stripe"
-  @version "2.17.3-2"
+  @version "2.17.3-3"
 
   def project do
     [
